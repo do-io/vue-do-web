a2f0f6c122ea952939f1ec8889f28f6223fdd2f0
Darren Odden <darren@odden.io>
2019-01-20 16:27:25 -0800

    run the font styling in app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link> 
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -9,20 +9,37 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+:root {
+  --dark-bg-color: #101656;
+  --dark-color: #FFD24C;
+  --dark-a-color: #2CCCF3;
+  --blue: #2CCCF3;
+  --dk-blue: #0F305C;
+}
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: var(--dark-color);
+  background-color: var(--dark-bg-color);
+}
+a {
+  color: var(--dark-a-color);
+  &:hover {
+    color: lighten(#2CCCF3,20%);
+  }
+}
+#app {
+  // font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
+  // color: #2c3e50;
 }
 #nav {
   padding: 30px;
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    margin-left: 1rem;
     &.router-link-exact-active {
-      color: #42b983;
+      color: darken(#2CCCF3, 20%);
     }
   }
 }
