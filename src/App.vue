c96e02aad92323d2ba506088944b0933330562dd
Darren Odden <darren@odden.io>
2019-01-24 08:18:34 -0800

    nav updates

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> 
-      <router-link to="/about">About</router-link>
-    </div>
+    <header>
+      <div id="logo">{{ title="Darren" }}</div>
+      <div id="nav">
+        <router-link to="/">Home</router-link> 
+        <router-link to="/about">About</router-link>
+        <font-awesome-icon :icon="['fal', 'search']" id="search" />
+      </div>
+    </header>
     <router-view/>
+    <footer>
+      <div id="info">
+        <font-awesome-icon :icon="['fab', 'linkedin']" />
+        <font-awesome-icon :icon="['fab', 'twitter']" />
+        <font-awesome-icon :icon="['fab', 'github']" />
+        <font-awesome-icon :icon="['fab', 'codepen']" />
+
+        <font-awesome-icon :icon="['fal', 'info-circle']" />
+      </div>
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
 :root {
   --dark-bg-color: #101656;
-  --dark-color: #FFD24C;
+  --dark-color: #2CCCF3;
   --dark-a-color: #2CCCF3;
   --blue: #2CCCF3;
   --dk-blue: #0F305C;
+  --green: #1DE9B6;
+  --pink: #F515CA;
+  --purple: #6002ee;
+  --red: #FF1744;
+  --yellow: #FFD24C;
+  --orange: #F57C00;
 }
 body {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -24,9 +44,14 @@ body {
   background-color: var(--dark-bg-color);
 }
 a {
-  color: var(--dark-a-color);
+  color: var(--yellow);
+  opacity: .8;
+  text-decoration: none;
+  transition-property: opacity;
+  transition-duration: .5s;
+  transition-timing-function: ease-in-out;
   &:hover {
-    color: lighten(#2CCCF3,20%);
+    opacity: 1;
   }
 }
 #app {
@@ -34,13 +59,51 @@ a {
   text-align: center;
   // color: #2c3e50;
 }
-#nav {
+header {
   padding: 30px;
+  display: grid;
+  grid-row: 1;
+  grid-template-columns: 1fr 2fr;
+
+}
+#logo {
+  text-align: left;
+}
+#nav {
+  color: var(--dark-color);
+  text-align: right;
   a {
+    color: var(--blue);
     margin-left: 1rem;
     &.router-link-exact-active {
-      color: darken(#2CCCF3, 20%);
+      opacity: .5;
     }
   }
+  #search {
+    margin-left: 30px;
+  }
+
+}
+footer {
+  #info {
+    opacity: .5;
+    padding-bottom: 30px;
+    background: transparent;
+    display: grid;
+    position:fixed;
+    right: 0;
+    bottom: 0;
+    width: 100px;
+    transition-property: opacity;
+    transition-duration: 1s;
+    transition-timing-function: ease-in-out;
+    svg {
+      padding: 10px 30px;
+    }
+    &:hover {
+      opacity: 1;
+    }
+  }
+
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch, faInfoCircle } from '@fortawesome/pro-light-svg-icons'
+import { faLinkedin, faTwitter, faGithub, faCodepen } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSearch, faInfoCircle, faLinkedin, faTwitter, faGithub, faCodepen)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false;
 
 new Vue({
