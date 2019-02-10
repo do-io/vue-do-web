6c1873e7ebfefdd313c21380e6c8c39b9cfb7a60
Darren Odden <darren@odden.io>
2019-02-10 11:37:33 -0800

    move header and footer into layout

---
+++ src/components/layout/FooterFull.vue
@@ -0,0 +1,55 @@
+<template>
+  <footer id="info">
+    <a href="https://linkedin.com/in/darrenodden" target="_blank">
+      <font-awesome-icon :icon="['fab', 'linkedin']"/>
+    </a>
+    <a href="https://twitter.com/darrenodden" target="_blank">
+      <font-awesome-icon :icon="['fab', 'twitter']"/>
+    </a>
+    <a href="https://github.com/do-io" target="_blank">
+      <font-awesome-icon :icon="['fab', 'github']"/>
+    </a>
+    <a href="https://codepen.io/DarrenOdden/" target="_blank">
+      <font-awesome-icon :icon="['fab', 'codepen']"/>
+    </a>
+    <a href="https://dribbble.com/darrenodden" target="_blank">
+      <font-awesome-icon :icon="['fab', 'dribbble']"/>
+    </a>
+    <a href="https://www.behance.net/DarrenOdden" target="_blank">
+      <font-awesome-icon :icon="['fab', 'behance']"/>
+    </a>
+
+    <font-awesome-icon :icon="['fal', 'info-circle']"/>
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  bottom: 0;
+  position: absolute;
+}
+#info {
+  opacity: 0.8;
+  padding-bottom: 20px;
+  background: transparent;
+  display: grid;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  transition-property: opacity;
+  transition-duration: 0.5s;
+  transition-timing-function: ease-in-out;
+  a {
+    color: var(--blue);
+  }
+  svg {
+    padding: 0.35rem 2.19rem;
+    &.fa-info-circle {
+      padding: 1rem 2.19rem 0.35rem;
+    }
+  }
+  &:hover {
+    opacity: 1;
+  }
+}
+</style>
---
+++ src/components/layout/HeaderFull.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <div id="logo">
+      <bydesign-logo/>
+    </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <font-awesome-icon :icon="['fal', 'search']" id="search"/>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  display: grid;
+  grid-row: 1;
+  grid-template-columns: 1fr 2fr;
+}
+#logo {
+  text-align: left;
+  padding: 14px;
+  > svg {
+    max-height: 80px;
+    min-width: 256px;
+  }
+}
+#nav {
+  padding: 2.19rem;
+  color: var(--dark-color);
+  text-align: right;
+  a {
+    color: var(--dark-color);
+    margin-left: 1rem;
+    &.router-link-exact-active {
+      opacity: 0.5;
+    }
+  }
+  #search {
+    margin-left: 30px;
+  }
+}
+</style>
